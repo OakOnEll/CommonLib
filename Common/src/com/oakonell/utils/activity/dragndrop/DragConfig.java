44da73b12f8addf9073f6dbec3a37cbc1a591c33
Rob O'Connell <oakOnEllGitHub@theoconnells.net>
2014-02-10 10:26:59 -0500

    Modify Dragging operations

---
+++ Common/src/com/oakonell/utils/activity/dragndrop/DragConfig.java
@@ -0,0 +1,8 @@
+package com.oakonell.utils.activity.dragndrop;
+
+public class DragConfig {
+	public int alpha = 100;
+	public boolean drawSelected= true;
+	public boolean vibrate = true;
+	public float animationScale = 0.9f;
+}
